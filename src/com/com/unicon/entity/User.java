690c3456a1b343a9ed828b01df466bb35006f9f0
Administrator <2929884253@qq.com>
2019-08-26 11:59:29 +0800

    用户哦1

--- src/com/com/unicon/entity/User.java
+++ src/com/com/unicon/entity/User.java
@@ -3,7 +3,7 @@ package com.unicon.entity;
 /**
  * @Author xyp
  */
-public class User<psvm> {
+public class User{
     public static void main(String[] args) {
         System.out.println(3211);
     }
